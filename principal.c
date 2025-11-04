1ee3eb51bcda9efe789466f80318b63a48c9bd86
mmyujdatov <mmyujdatov@gmail.com>
2025-11-04 10:29:51 +0100

    Commit de prueba

---
+++ principal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "sumar.h"
+
+//Función del principal
+
+int main() {
+
+	int a = 5, b = 10, c;
+	
+	c = sumar(a,b);
+
+	printf("El resultado de la suma es: %d\n", c);
+
+	return 0;
+}
